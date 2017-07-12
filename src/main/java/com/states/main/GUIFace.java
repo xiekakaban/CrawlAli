801120e097bc411b6a1849a373097432645bbab9
xiekakaban <ruantianbo@163.com>
2017-07-12 17:55:53 +0800

    change to javaFX

--- src/main/java/com/states/main/GUIFace.java
+++ src/main/java/com/states/main/GUIFace.java
@@ -1,116 +1,176 @@
 package com.states.main;
 
+
 import com.states.db.ProductDBRunner;
 import com.states.entity.ProductEntity;
 import com.states.util.Constants;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.util.Callback;
 
-import javax.swing.*;
-import javax.swing.border.Border;
-import java.awt.*;
-import java.util.*;
-import java.util.List;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 
 /**
  * Created by e604845 on 7/5/2017.
  */
-public class GUIFace {
+public class GUIFace extends Application{
 
-    JFrame mainFrame;
-    ProductDBRunner productDBRunner = new ProductDBRunner();
-    public GUIFace(){}
+    public GUIFace() {
+
+    }
 
     public void initAndShow(){
-        initGUI();
-        this.mainFrame.pack();
-        this.mainFrame.setVisible(true);
-    }
-    public void initGUI(){
-        mainFrame = new JFrame("AliCrawler");
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Container mainPanel = mainFrame.getContentPane();
-        mainFrame.setSize(new Dimension(500, 700));
-        JPanel detailPanel = createDetailPanel();
-        JPanel prodSelectPanel = createProdSelectPanel();
-        JPanel commandPanel = createCommandPanel();
-        GridBagLayout gridBagLayout = new GridBagLayout();
-
-        mainPanel.setLayout(gridBagLayout);
-
-        GridBagConstraints gridBagConstraints = new GridBagConstraints();
-
-        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        mainPanel.add(detailPanel,gridBagConstraints);
-
-
-        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 0;
-        mainPanel.add(prodSelectPanel,gridBagConstraints);
-
-
-        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.gridwidth=2;
-        mainPanel.add(commandPanel,gridBagConstraints);
-    }
-    private JPanel createDetailPanel(){
-        JPanel dPanel = new JPanel();
-        dPanel.setPreferredSize(new Dimension(500, 600));
-        dPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY));
-        dPanel.add(new JLabel("DetailPanel"));
-        return  dPanel;
+        launch();
     }
 
-    private JPanel createProdSelectPanel(){
-        JPanel pPanel = new JPanel();
-        pPanel.setPreferredSize(new Dimension(200, 600));
-        pPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Ali Crawler");
+        BorderPane mainBorderlayout = new BorderPane();
+        mainBorderlayout.setBottom(initBottom());
+        mainBorderlayout.setCenter(initCenter());
+        mainBorderlayout.setRight(initRight());
+        Scene scene = new Scene(mainBorderlayout,800,750);
+        scene.getStylesheets().add(Constants.resourcePath+ File.separator+"css"+File.separator+"default.css");
 
-        pPanel.setLayout(new BorderLayout());
-        List<ProductEntity> productNameList = productDBRunner.getProdNameList();
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-        Collections.sort(productNameList, new Comparator<ProductEntity>() {
+    private Pane initCenter(){
+        Label centerL = new Label("Center");
+        GridPane gridPane = new GridPane();
+        gridPane.add(centerL, 1, 0);
+        gridPane.setPrefHeight(600);
+        gridPane.setPrefWidth(500);
+        gridPane.setStyle("-fx-background-color: #891111;");
+        return gridPane;
+    }
+
+    private Pane initBottom(){
+        HBox hBox = new HBox();
+        Button fetchBth = new Button("Bottom");
+        hBox.getChildren().add(fetchBth);
+        AnchorPane anchorPane = new AnchorPane();
+        anchorPane.getChildren().add(hBox);
+        anchorPane.setBottomAnchor(hBox,8.0);
+        anchorPane.setRightAnchor(hBox, 5.0);
+        anchorPane.setPrefHeight(150);
+        anchorPane.setStyle("-fx-background-color: #88ff99;");
+
+        return anchorPane;
+    }
+    private Pane initRight(){
+        VBox vBox = new VBox();
+        ListView<ProductEntity> list = new ListView<ProductEntity>();
+        list.setItems(getProductItems());
+
+        list.setCellFactory(new Callback<ListView<ProductEntity>, ListCell<ProductEntity>>() {
             @Override
-            public int compare(ProductEntity o1, ProductEntity o2) {
-                return o1.getId().compareTo(o2.getId());
+            public ListCell<ProductEntity> call(ListView<ProductEntity> param) {
+                ProductListCell cell = new ProductListCell();
+
+                return cell;
             }
         });
-        DefaultListModel<ProductEntity> l1 = new DefaultListModel<>();
-        for(ProductEntity item : productNameList) {
-            l1.addElement(item);
+
+        list.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                onClickListItem(list.getSelectionModel().getSelectedItem().getId());
+                System.out.println(list.getSelectionModel().getSelectedItem().getTitle()+" has been selected!" + System.currentTimeMillis());
+            }
+        });
+
+        list.setPrefHeight(600);
+        vBox.getChildren().add(list);
+        vBox.setPrefHeight(600);
+        vBox.setPrefWidth(300);
+        vBox.setStyle("-fx-background-color: #336699;");
+        return vBox;
+    }
+
+    private ObservableList<ProductEntity> getProductItems(){
+        ObservableList<ProductEntity> items = FXCollections.observableArrayList();
+        ProductDBRunner productDBRunner = new ProductDBRunner();
+        for(ProductEntity pItem : productDBRunner.getProdNameList()){
+            items.add(pItem);
         }
-        JList prodList = new JList(l1);
-
-        prodList.setCellRenderer(new ProductCellListRenderer());
-//
-       pPanel.add(prodList, BorderLayout.CENTER);
-        return  pPanel;
+        return items;
     }
 
-    private JPanel createCommandPanel(){
-        JPanel cPanel = new JPanel();
-        cPanel.setPreferredSize(new Dimension(500, 100));
-        cPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY));
-        JLabel s = new JLabel("CommandPanel");
-        s.setBackground(Color.green);
-        s.setForeground(Color.red);
+    private void onClickListItem(String clickId){
+        GridPane gridPane = new GridPane();
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
 
-        s.setIcon(Constants.ISADDICON);
-        cPanel.add(s);
-        return  cPanel;
+        //title
+
+        //picList
+
+        //prodDetEntity
+
+        //detail
+
+        //content
+
+        //createTime
+
+
     }
 
+    class ProductListCell extends ListCell<ProductEntity>{
 
-    private Component getCompByName(Container container ,String name){
-        if(container == null ) return null;
-        for(Component comp : container.getComponents()){
-            if(comp.getName().equals(name)){
-                return  comp;
+        @Override
+        protected void updateItem(ProductEntity item, boolean empty) {
+            super.updateItem(item, empty);
+            if(item != null) {
+                HBox listHBox = new HBox();
+                String title = item.getTitle();
+                if(title.length()>40){
+                    title = title.substring(40);
+                }
+                Label prodTitle = new Label(title);
+                prodTitle.setPrefWidth(250);
+                prodTitle.setWrapText(true);
+                Image isAddIcon = null;
+                try {
+                    isAddIcon = new Image(new FileInputStream(Constants.resourcePath+ File.separator+"images"+File.separator+"arrow_right.png"),20,20,false,false);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                ImageView imageView = new ImageView(isAddIcon);
+
+                Label isAddLabel = new Label();
+                isAddLabel.setStyle("-fx-padding: 5 0 0 10");
+                if(item.getIsAdd().equals("1")) {
+                    isAddLabel.setGraphic(new ImageView(isAddIcon));
+                }
+
+                listHBox.getChildren().addAll(prodTitle, isAddLabel);
+
+                setGraphic(listHBox);
             }
         }
-        return null;
     }
+
+
 }
--- src/main/java/com/states/util/Constants.java
+++ src/main/java/com/states/util/Constants.java
@@ -9,6 +9,7 @@ import java.io.File;
 public class Constants {
     public static final Boolean isLocalTest = Boolean.TRUE; //本地测试
     public static final String currentProjectPath = System.getProperty("user.dir");
+    public static final String resourcePath = currentProjectPath+File.separator+"src"+File.separator+"resources";
     public static final ImageIcon ISADDICON = new ImageIcon(Constants.currentProjectPath+ File.separator+"src"+File.separator+"resources"+File.separator+"array_right.png");
 
 }
